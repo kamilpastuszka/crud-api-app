b3fc3838ef0fbccea86a68429ada83d29a24db6e
Kamil Pastuszka <kamil_p7@o2.pl>
2018-07-09 09:46:06 +0100

    refactoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
-import logo from '../assets/cake-logo.jpg';
 
 export const Header = () => {
   return (
       <div className="row header">
         <div className="col-3">
-         <span className="logo-image"><img src={logo} alt="cake-logo"/></span>
+         <span className="logo-image"></span>
         </div>
         <div className="col-3">
          <Link to='/cakes/add' className='btn btn-lg btn-primary'>
